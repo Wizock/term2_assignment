92e69f05d2219568b4e00b4ef8f838e817c65d78
Wizock <36351054+Wizock@users.noreply.github.com>
2021-03-18 11:02:19 +1000

    first commit

---
+++ db_handles.py
@@ -0,0 +1,40 @@
+import os, sqlite3
+
+def db_var():
+    try:
+        con = sqlite3.connect(os.getcwd() + '/example.db')
+    except sqlite3.OperationalError as e:
+      if e.args[0].startswith('no such table'):
+        exists = False
+      else:
+        raise
+    return con
+
+def create_table():
+    con = db_var()
+    con.execute('''
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+            )
+            ''')
+    con.commit()
+    con.close()
+
+create_table()
+
+def register_user(email, username, password, password2):
+  try:
+    db = db_var()
+    db.execute("INSERT INTO users (email, username, password) VALUES(?,?,?)",(email,username,password))
+  except sqlite3.OperationalError as e:
+      print(e)
+
+def login_user(email, password):
+  try:
+    db = db_var()
+    db.execute("SELECT email, password FROM users WHERE email = ? AND password = ?",(email, password))
+  except sqlite3.OperationalError as e:
+      print(e)
---
+++ main.py
@@ -0,0 +1,44 @@
+import sqlite3
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from db_handles import *
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        db = db_var()
+        check_email = db.execute("SELECT * FROM users WHERE email= ?",(email))
+        check_password = db.execute("SELECT * FROM users WHERE password = ?",(password))
+        if account_check is not None:
+            flash('You were successfully logged')
+    else:
+        return render_template('login.html')
+
+@app.route('/reg', methods=("GET", "POST"))
+def reg():
+    if request.method == "POST":
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+
+        if email is not None and username is not None:
+            if password == password2 or password2 == password:
+                register_user(email, username, password, password2)
+                return redirect('/login')
+    else:
+        return render_template('register.html', title="login")
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
