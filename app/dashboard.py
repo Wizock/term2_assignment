3e60252ffb7f80a6446e93ca30539065200f2d42
Wizock <Rohaanerodasahmed@gmail.com>
2021-03-25 21:30:53 +1000

    idk

--- app/dashboard.py
+++ app/dashboard.py
@@ -2,8 +2,21 @@ from flask import Blueprint, render_template
 
 dashboard = Blueprint('views', __name__)
 
+#used to do CRUD for user to enter thier premiers reading challange books. 
+
 @dashboard.route("/")
 @dashboard.route('/home')
+def home():
+    return render_template('index.html')
 
+@dashboard.route('/home')
+def home():
+    return render_template('index.html')
+
+@dashboard.route('/home')
+def home():
+    return render_template('index.html')
+
+@dashboard.route('/home')
 def home():
     return render_template('index.html')
