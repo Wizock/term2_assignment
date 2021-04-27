4905daa3d12bb267149ad4999bbd2d314466ddbd
Wizock <Rohaanerodasahmed@gmail.com>
2021-04-28 02:15:50 +1000

    forced the routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,8 @@ def create_app():
     app = Flask(__name__)
     from .dashboard import dashboard # profile pages, personal infomation and data access
     from .auth import auth # small routing group | used to authenticate or register a user
-    from .library import library as libraryviews # library data operation routes. 
     app.secret_key = '76d9a377304d34988ab3e10dc'
     app.register_blueprint(dashboard, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(libraryviews, url_prefix='/')
-
     return app
 
--- app/dashboard.py
+++ app/dashboard.py
@@ -8,7 +8,9 @@ from flask import Blueprint, render_template
 from flask.globals import request, session
 from .entry_form import EntryForm
 import datetime
-
+from app.database.db_handles import libquery, nOflibs
+from flask import Flask, render_template, request, redirect, Blueprint
+import datetime
 dashboard = Blueprint('views', __name__)
 
 #used to do CRUD for user to enter thier premiers reading challange books. 
@@ -97,6 +99,35 @@ def list():
     Numberofpages = Numberofpages,
 
     )
+
+@dashboard.route('/library', methods=["GET", "POST"])
+def library():
+    if request.method == "POST":
+        postcodevar  = request.forms['postcode']
+        Entryid = nOflibs(postcodevar)
+        return render_template(
+            "library/library.html",
+            dataisentered=True ,
+            Entryid=Entryid ,
+            postcode = postcodevar,
+            BranchName         =  libquery(postcodevar,'BranchName'         ),
+            WiFiAvailability   =  libquery(postcodevar,'WiFiAvailability'   ),
+            AddressLine1       =  libquery(postcodevar,'AddressLine1'       ),
+            Postcode           =  libquery(postcodevar,'Postcode'           ),
+            Contact            =  libquery(postcodevar,'Contact'            ),
+            BranchManagerName  =  libquery(postcodevar,'BranchManagerName'  ),
+            BranchManagerEmail =  libquery(postcodevar,'BranchManagerEmail' ),
+            Phone              =  libquery(postcodevar,'Phone'              ),
+            Monday             =  libquery(postcodevar,'Monday'             ),
+            Tuesday            =  libquery(postcodevar,'Tuesday'            ),
+            Wednesday          =  libquery(postcodevar,'Wednesday'          ),
+            Thursday           =  libquery(postcodevar,'Thursday'           ),
+            Friday             =  libquery(postcodevar,'Friday'             ),
+            Saturday           =  libquery(postcodevar,'Saturday'           ),
+            Sunday             =  libquery(postcodevar,'Sunday'             ),
+            )
+    else:
+        return render_template("library/library.html",dataisentered=False)
 
 # @dashboard.route("/debug")
 # def debug():
